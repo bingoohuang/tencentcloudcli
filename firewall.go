0f31da396e58264a2d9f98fa957c1dea1db16283
huangjinbing <bingoo.huang@gmail.com>
2023-12-25 10:15:51 +0800

    自动拉起 code 编辑防火墙规则，并且命令行确认修改

--- firewall.go
+++ firewall.go
@@ -3,9 +3,12 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"os"
-	"time"
 
+	"github.com/bingoohuang/gum/confirm"
+	"github.com/commander-cli/cmd"
+	"github.com/imroc/req/v3"
 	lighthouse "github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/lighthouse/v20200324"
 )
 
@@ -21,46 +24,9 @@ type InstanceFirewallRules struct {
 
 func (r *FirewallCmd) Run(*Context) error {
 	if r.File != "" {
-		return r.modifyRules()
+		return r.modifyRules(r.File)
 	}
 	return r.listRules()
-}
-
-func (r *FirewallCmd) modifyRules() error {
-	if _, err := os.Stat(r.File); err != nil {
-		return err
-	}
-
-	fileData, err := os.ReadFile(r.File)
-	if err != nil {
-		return err
-	}
-
-	var rules InstanceFirewallRules
-	if err := json.Unmarshal(fileData, &rules); err != nil {
-		return err
-	}
-
-	req := lighthouse.NewModifyFirewallRulesRequest()
-	req.InstanceId = rules.InstanceId
-	for _, rule := range rules.Rules {
-		req.FirewallRules = append(req.FirewallRules, &lighthouse.FirewallRule{
-			Protocol:                rule.Protocol,
-			Port:                    rule.Port,
-			CidrBlock:               rule.CidrBlock,
-			Action:                  rule.Action,
-			FirewallRuleDescription: rule.FirewallRuleDescription,
-		})
-	}
-
-	rsp, err := getClient().ModifyFirewallRules(req)
-	if err != nil {
-		return err
-	}
-
-	resp, _ := json.MarshalIndent(rsp, "", "    ")
-	fmt.Printf("%s\n", resp)
-	return nil
 }
 
 func (r *FirewallCmd) listRules() error {
@@ -92,17 +58,94 @@ func (r *FirewallCmd) listRules() error {
 		})
 	}
 
+	type PingResult struct {
+		RemoteAddr   string `json:"RemoteAddr"`
+		ForwardedFor string `json:"X-Forwarded-For"`
+		RealIP       string `json:"X-Real-IP"`
+	}
+
+	var pingResult PingResult
+	reqClient.R().SetSuccessResult(&pingResult).Get("https://d5k.top/ping")
+	pingResultJSON, _ := json.Marshal(pingResult)
+	log.Printf("ping: %s", pingResultJSON)
+
 	jsonRules, err := json.MarshalIndent(rules, "", "    ")
 	if err != nil {
 		return err
 	}
 
-	jsonFile := "firewall_rules_" + time.Now().Format(`20060102150405`) + ".json"
-	if err := os.WriteFile(jsonFile, jsonRules, os.ModePerm); err != nil {
-		fmt.Printf("%s\n", jsonFile)
-	} else {
-		fmt.Printf("%s 已生成，请修改此文件后执行 `tencentcloudcli firewall --file=%s` 完成防火墙规则修改!\n", jsonFile, jsonFile)
+	// Create a temporary file
+	file, errs := os.CreateTemp("", "temp-*.txt")
+	if errs != nil {
+		return err
 	}
 
+	defer file.Close()
+
+	if _, err := file.Write(jsonRules); err != nil {
+		return err
+	}
+
+	file.Close()
+
+	log.Printf("请修改防火墙规则: %s", file.Name())
+
+	c := cmd.NewCommand("code " + file.Name())
+	if err = c.Execute(); err == nil {
+		confirmOptions := &confirm.Options{}
+		confirm, err := confirmOptions.Confirm("确认修改防火墙规则么?")
+		if err != nil {
+			return err
+		}
+
+		if confirm == "YES" {
+			if err := r.modifyRules(file.Name()); err != nil {
+				return err
+			}
+			os.Remove(file.Name())
+			return err
+		}
+	}
+
+	fmt.Printf("单独执行 `tencentcloudcli firewall --file=%s` 完成防火墙规则修改!\n", file.Name())
+	return nil
+}
+
+var reqClient = req.C()
+
+func (r *FirewallCmd) modifyRules(file string) error {
+	if _, err := os.Stat(file); err != nil {
+		return err
+	}
+
+	fileData, err := os.ReadFile(file)
+	if err != nil {
+		return err
+	}
+
+	var rules InstanceFirewallRules
+	if err := json.Unmarshal(fileData, &rules); err != nil {
+		return err
+	}
+
+	rq := lighthouse.NewModifyFirewallRulesRequest()
+	rq.InstanceId = rules.InstanceId
+	for _, rule := range rules.Rules {
+		rq.FirewallRules = append(rq.FirewallRules, &lighthouse.FirewallRule{
+			Protocol:                rule.Protocol,
+			Port:                    rule.Port,
+			CidrBlock:               rule.CidrBlock,
+			Action:                  rule.Action,
+			FirewallRuleDescription: rule.FirewallRuleDescription,
+		})
+	}
+
+	rsp, err := getClient().ModifyFirewallRules(rq)
+	if err != nil {
+		return err
+	}
+
+	resp, _ := json.Marshal(rsp)
+	log.Printf("ModifyFirewallRules: %s", resp)
 	return nil
 }
